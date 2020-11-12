d5b126e68a44355c4c649172b2bd5670a94d38f7
Zac Kolton <koltonz@myumanitoba.ca>
2020-11-11 23:00:42 -0600

    Added stuff

---
+++ Website/Scripts/test.js
@@ -0,0 +1,11 @@
+function funct(input) {
+    console.log(input);
+}
+
+/*
+          <p> Test input</p>
+          <form id="form">
+            Input: <input type="text" name = "input">
+            <input type = "button" onclick="funct(input.value)" value = "submit">
+          </form>
+          */
